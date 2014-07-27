c846a755ff763e1704bd6900b4a61922fd71e104
Trisco7 <trisco7@yahoo.com>
2014-07-27 22:52:43 +0530

    Update cachematrix.R
    Assignment Program 2 with updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,26 +3,26 @@
 ### cache result instead of calculating again and again whenever the 
 ### inverse for a given matrix needs to be found out. For this purpose, 
 ### two functions have been written.
-### 1.makeCacheMatrix: This function creates a special "matrix" object that 
+### 1. makeCacheMatrix(): This function creates a special "matrix" object that 
 ### can cache its inverse.
-### 2.cacheSolve: This function computes the inverse of the special "matrix" 
-### returned by makeCacheMatrix above. 
+### 2. cacheSolve(): This function computes the inverse of the special "matrix" 
+### returned by "makeCacheMatrix()" above. 
 ### If the inverse has already been calculated (and the matrix has not changed), 
-### then the cachesolve should retrieve the inverse from the cache.
+### then the "cachesolve()" should retrieve the inverse from the cache.
 
 
 ## The function "makeCacheMatrix()" takes a matrix as its input and assumes that
 ## the matrix supplied is always invertible. The function creates a special 
 ## "matrix" object and returns a list of members and/or methods such as 
-## set(): to set the value of matrix
-## get(): to get the value of matrix
-## setinverse(): to set the inverse value of matrix
-## getinverse(): to get the inverse value of matrix
-## The "<<-" operator has been used in set() and setinverse() functions causing a 
-## search to made through parent environments for an existing definition of the 
+## 1. set(): to set the value of matrix
+## 2. get(): to get the value of matrix
+## 3. setinverse(): to set the inverse value of matrix
+## 4. getinverse(): to get the inverse value of matrix
+## The "<<-" operator has been used in "set()" and "setinverse()" functions causing a 
+## search to be made through parent environments for an existing definition of the 
 ## variable being assigned. If such a variable is found, its value is redefined 
 ## instead of allocating new space to the variables.
-## The variables returned in get() and getinverse() functions get respective 
+## The variables returned in "get()" and "getinverse()" functions get respective 
 ## values due to lexical scoping rules followed in R.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -37,19 +37,18 @@ makeCacheMatrix <- function(x = matrix()) {
        list(set = set, get = get,
             setinverse = setinverse,
             getinverse = getinverse)
-
 } #makeCacheMatrix 
 
 
 ## The function "cacheSolve()" takes the special "matrix" object created in the 
 ## function "makeCacheMatrix()" as its input and returns the inverse of the matrix 
 ## stored in the object. First time, the function computes the inverse of the matrix 
-## retrieved using the member function get() of the list object and set the inverse 
+## retrieved using the member function "get()" of the list object and set the inverse 
 ## in the object by accessing the member function setinverse() of the list. 
 ## Then for each next time, if matrix has not been changed, the cached inverse result 
-## is retrieved using the member function getinverse() of the list object. 
+## is retrieved using the member function "getinverse()" of the list object. 
 ## (This function returns NULL the very first time. Hence, inverse needs to be 
-## calculated using solve())
+## calculated using "solve()")
 
 cacheSolve <- function(x, ...) {
         
